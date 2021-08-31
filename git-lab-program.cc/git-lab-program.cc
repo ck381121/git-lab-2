d30c9bcb77f88924d3b78b667c7ea78833adf6bb
Christian Kuhlwein <ck381121@ohio.edu>
2021-08-31 17:43:13 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc/git-lab-program.cc
@@ -0,0 +1,21 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Christian Kuhlwein>
+ *        Date: <August 31st, 2021>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+
+    cout << "Hello Git!" << endl;
+
+    return 0;
+}// main
